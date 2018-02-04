1396d5c21ec1e690e8f90fd031e76123a47da259
王俊超 <shining-glory@qq.com>
2018-02-04 20:50:15 +0800

    commit

--- mango-netty-rpc-kryo/src/main/java/mango/rpc/serializer/KryoReflectionFactory.java
+++ mango-netty-rpc-kryo/src/main/java/mango/rpc/serializer/KryoReflectionFactory.java
@@ -1,6 +1,14 @@
 package mango.rpc.serializer;
 
-import com.esotericsoftware.kryo.Kryo;
+
+import de.javakaffee.kryoserializers.*;
+import mango.rpc.context.RpcRequest;
+import mango.rpc.context.RpcResponse;
+
+import java.lang.reflect.InvocationHandler;
+import java.net.URI;
+import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * <pre>
@@ -12,5 +20,26 @@ import com.esotericsoftware.kryo.Kryo;
  * Github: https://github.com/wang-jun-chao
  * All Rights Reserved !!!
  */
-public class KryoReflectionFactory extends Kryo {
+public class KryoReflectionFactory extends KryoReflectionFactorySupport {
+    public KryoReflectionFactory() {
+        setRegistrationRequired(false);
+        setReferences(true);
+        register(RpcRequest.class, new RpcRequestSerializer());
+        register(RpcResponse.class, new RpcResponseSerializer());
+        register(Arrays.asList("").getClass(), new ArraysAsListSerializer());
+        register(Collections.EMPTY_LIST.getClass(), new CollectionsEmptyListSerializer());
+        register(Collections.EMPTY_MAP.getClass(), new CollectionsEmptyMapSerializer());
+        register(Collections.EMPTY_SET.getClass(), new CollectionsEmptySetSerializer());
+        register(Collections.singletonList("").getClass(), new CollectionsSingletonListSerializer());
+        register(Collections.singleton("").getClass(), new CollectionsSingletonSetSerializer());
+        register(Collections.singletonMap("", "").getClass(), new CollectionsSingletonMapSerializer());
+        register(Pattern.class, new RegexSerializer());
+        register(BitSet.class, new BitSetSerializer());
+        register(URI.class, new URISerializer());
+        register(UUID.class, new UUIDSerializer());
+        register(GregorianCalendar.class, new GregorianCalendarSerializer());
+        register(InvocationHandler.class, new JdkProxySerializer());
+        UnmodifiableCollectionsSerializer.registerSerializers(this);
+        SynchronizedCollectionsSerializer.registerSerializers(this);
+    }
 }
