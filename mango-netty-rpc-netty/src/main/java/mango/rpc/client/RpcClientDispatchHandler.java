cd6a85d3ed4ce363fd6e8580c98984d2341c1f59
王俊超 <shining-glory@qq.com>
2018-02-04 21:17:33 +0800

    commit

---
+++ mango-netty-rpc-netty/src/main/java/mango/rpc/client/RpcClientDispatchHandler.java
@@ -0,0 +1,14 @@
+package mango.rpc.client;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ * Author: 王俊超
+ * Date: 2018-02-04 21:16
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class RpcClientDispatchHandler {
+}
