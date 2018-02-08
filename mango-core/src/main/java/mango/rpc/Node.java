270a114b24e1406b9149b97a00443704f09fd8ca
王俊超 <shining-glory@qq.com>
2018-02-08 21:02:00 +0800

    commit

--- mango-core/src/main/java/mango/rpc/Node.java
+++ mango-core/src/main/java/mango/rpc/Node.java
@@ -3,16 +3,38 @@ package mango.rpc;
 import mango.common.URL;
 
 /**
+ * 服务节点对象
  */
 public interface Node {
 
+    /**
+     * 初始化
+     */
     void init();
 
+    /**
+     * 销毁
+     */
     void destroy();
 
+    /**
+     * 当前状态是否可用
+     *
+     * @return
+     */
     boolean isAvailable();
 
+    /**
+     * 返回当前节点的描述信息
+     *
+     * @return
+     */
     String desc();
 
+    /**
+     * 获取节点相关的URL
+     *
+     * @return
+     */
     URL getUrl();
 }
