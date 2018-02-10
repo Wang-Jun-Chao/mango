98509b5dc25fde359a40c376a738ff49ddf3659e
王俊超 <shining-glory@qq.com>
2018-02-10 08:09:24 +0800

    commit

--- mango-core/src/main/java/mango/util/IoUtils.java
+++ mango-core/src/main/java/mango/util/IoUtils.java
@@ -6,18 +6,33 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * @author Ricky Fung
+ * IO工具
  */
 public class IoUtils {
 
+    /**
+     * 关闭输入流
+     *
+     * @param input
+     */
     public static void closeQuietly(InputStream input) {
         closeQuietly((Closeable) input);
     }
 
+    /**
+     * 关闭输出流
+     *
+     * @param output
+     */
     public static void closeQuietly(OutputStream output) {
         closeQuietly((Closeable) output);
     }
 
+    /**
+     * 关闭流
+     *
+     * @param closeable
+     */
     public static void closeQuietly(Closeable closeable) {
         try {
             if (closeable != null) {
@@ -28,22 +43,53 @@ public class IoUtils {
         }
     }
 
+    /**
+     * 关闭流
+     *
+     * @param closeables
+     */
     public static void closeQuietly(Closeable... closeables) {
         for (Closeable closeable : closeables) {
             closeQuietly(closeable);
         }
     }
 
+    /**
+     * 将输入流的内容读取到输出流中
+     *
+     * @param in
+     * @param out
+     * @param bufferSize
+     * @return
+     * @throws IOException
+     */
     public static long copy(InputStream in, OutputStream out, int bufferSize) throws IOException {
         byte[] buff = new byte[bufferSize];
         return copy(in, out, buff);
     }
 
+    /**
+     * 将输入流的内容读取到输出流中
+     *
+     * @param in
+     * @param out
+     * @return
+     * @throws IOException
+     */
     public static long copy(InputStream in, OutputStream out) throws IOException {
         byte[] buff = new byte[1024];
         return copy(in, out, buff);
     }
 
+    /**
+     * 将输入流的内容读取到输出流中
+     *
+     * @param in
+     * @param out
+     * @param buff
+     * @return
+     * @throws IOException
+     */
     public static long copy(InputStream in, OutputStream out, byte[] buff) throws IOException {
         long count = 0;
         int len = -1;
--- mango-core/src/main/java/mango/util/NetUtils.java
+++ mango-core/src/main/java/mango/util/NetUtils.java
@@ -9,36 +9,71 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
- * @author Ricky Fung
+ * 网络工具类
  */
 public class NetUtils {
+    /**
+     * 本机地址
+     */
     public static final String LOCALHOST = "127.0.0.1";
+    /**
+     * 任播地址
+     */
     public static final String ANYHOST = "0.0.0.0";
     private static final Logger logger = LoggerFactory.getLogger(NetUtils.class);
+    /**
+     * 地址正则表达式
+     */
     private static final Pattern ADDRESS_PATTERN = Pattern.compile("^\\d{1,3}(\\.\\d{1,3}){3}\\:\\d{1,5}$");
+    /**
+     * IP正则表达式
+     */
     private static final Pattern IP_PATTERN = Pattern.compile("\\d{1,3}(\\.\\d{1,3}){3,5}$");
+    /**
+     * 本机地址
+     */
     private static volatile InetAddress LOCAL_ADDRESS = null;
 
+    /**
+     * 根据地址字符串获取网络地址对象
+     *
+     * @param addr
+     * @return
+     */
     public static InetSocketAddress parseSocketAddress(final String addr) {
         String[] arr = addr.split(":");
         InetSocketAddress isa = new InetSocketAddress(arr[0], Integer.parseInt(arr[1]));
         return isa;
     }
 
+    /**
+     * 获取本机地址
+     *
+     * @return
+     */
     public static InetAddress getLocalAddress() {
         return getLocalAddress(null);
     }
 
+    /**
+     * 获取本机地址
+     *
+     * @param destHostPorts
+     * @return
+     */
     public static InetAddress getLocalAddress(Map<String, Integer> destHostPorts) {
         if (LOCAL_ADDRESS != null) {
             return LOCAL_ADDRESS;
         }
 
+        // 根据本地主机名获取
         InetAddress localAddress = getLocalAddressByHostname();
         if (!isValidAddress(localAddress)) {
+            // 根据输入的ip和地址信息获取本地地址
             localAddress = getLocalAddressBySocket(destHostPorts);
         }
 
+        // 根据网络接口获取地址
         if (!isValidAddress(localAddress)) {
             localAddress = getLocalAddressByNetworkInterface();
         }
@@ -50,6 +85,11 @@ public class NetUtils {
         return localAddress;
     }
 
+    /**
+     * 根据主机名获取网络地址对象
+     *
+     * @return
+     */
     private static InetAddress getLocalAddressByHostname() {
         try {
             InetAddress localAddress = InetAddress.getLocalHost();
@@ -62,11 +102,18 @@ public class NetUtils {
         return null;
     }
 
+    /**
+     * 获取网络地址
+     *
+     * @param destHostPorts key:IP地址， value:端口号
+     * @return
+     */
     private static InetAddress getLocalAddressBySocket(Map<String, Integer> destHostPorts) {
         if (destHostPorts == null || destHostPorts.size() == 0) {
             return null;
         }
 
+        // 找到一个地址就返回
         for (Map.Entry<String, Integer> entry : destHostPorts.entrySet()) {
             String host = entry.getKey();
             int port = entry.getValue();
@@ -90,6 +137,11 @@ public class NetUtils {
         return null;
     }
 
+    /**
+     * 根据网络接口获取网络地址对象
+     *
+     * @return
+     */
     private static InetAddress getLocalAddressByNetworkInterface() {
         try {
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
@@ -119,12 +171,26 @@ public class NetUtils {
         return null;
     }
 
+    /**
+     * 验证地址是否符格式
+     *
+     * @param address
+     * @return
+     */
     public static boolean isValidAddress(String address) {
         return ADDRESS_PATTERN.matcher(address).matches();
     }
 
+    /**
+     * 验证地址是否正确，地址非空，非127.0.0.1，非任播地址
+     *
+     * @param address
+     * @return
+     */
     public static boolean isValidAddress(InetAddress address) {
-        if (address == null || address.isLoopbackAddress()) return false;
+        if (address == null || address.isLoopbackAddress()) {
+            return false;
+        }
         String name = address.getHostAddress();
         return (name != null && !ANYHOST.equals(name) && !LOCALHOST.equals(name) && IP_PATTERN.matcher(name).matches());
     }
